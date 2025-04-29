8e4b83db399aff14a6068889f8a74fc269454f0d
javierdastas <javier.dastas@outlook.com>
2025-04-29 10:38:04 -0400

    Update files

--- 06. session/app.py
+++ 06. session/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, session
 app = Flask(__name__)
 
-
 app.secret_key = "clave_secreta"
 
 @app.route("/login")
 def login():
     session["usuario"] = "Juan"
     return "Usuario autenticado"
+
+@app.route("/user")
+def get_user():
+    return session.get("usuario")
 
 @app.route("/logout")
 def logout():
